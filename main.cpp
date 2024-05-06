9dd2ce0b32d6cfe9c34a0a913ffbf3cfefb352e0
potomushozhenya <zhestokanov2017@yandex.ru>
2024-05-06 15:00:39 +0300

    Final solution

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ void modify(unsigned short* t, int &n, int&m, int p, int value) {  // set value 
     for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = (t[p] * t[p^1])%m;
 }
 
-int query(int l, int r, const unsigned short* t, int &n, int &m) {  // sum on interval [l, r)
-    int res = 1;
-    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+int query(int l, int r, const unsigned short* t, int &n, int &m) {  // mod multiply on interval [l, r]
+    unsigned short res = 1;
+    for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
         if (l&1) res = (res*t[l++])%m;
-        if (r&1) res = (res*t[--r])%m;
+        if (!(r&1)) res = (res*t[r--])%m;
     }
     return res;
 }
@@ -23,7 +23,7 @@ int main() {
     for (int i = 0; i < t; ++i) {
         int n,m;
         std::cin>>n>>m;
-        unsigned short a[2*n+1];
+        unsigned short a[2*n];
         char s[n];
         for (int j = 0; j < n; ++j) {
             std::cin>>a[j+n];
@@ -33,17 +33,13 @@ int main() {
             std::cin>>s[j];
         }
         build(a,n,m);
-        int l=1,r=n;
+        int l=0,r=n;
         int c=0;
-        query(1,n,a,n,m);
-        while (l<=r){
+        while (c<n){
+            std::cout<<query(l,r-1,a,n,m)<<' ';
             if (s[c]=='L'){
-                modify(a,n,m,l,1);
-                std::cout<<a[1]<<' ';
                 ++l;
             } else{
-                modify(a,n,m,r,1);
-                std::cout<<a[1]<<' ';
                 --r;
             }
             ++c;
